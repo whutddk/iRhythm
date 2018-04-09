46e471eb7c6aa692839a2f63bdb45ad0517072f3
Wuhan_University_of_Technology_Ruige_Lee <295054118@whut.edu.cn>
2018-04-09 21:13:04 +0800

    开始调SPI

---
+++ src/MUSIC/music.c
@@ -0,0 +1,14 @@
+#include "embARC.h"
+#include "embARC_debug.h"
+
+#include "include.h"
+
+void spi_poll()
+{
+	FS_SDCARD_SPI_CTRL_PTR sd_ctrl;
+	DEV_SPI_PTR spi;
+	spi =  spi_get_dev(sd_ctrl->spi_master);
+
+
+	
+}
