f28579cd7572248850d1e6043346f60724a2910a
Wuhan_University_of_Technology_Ruige_Lee <295054118@whut.edu.cn>
2018-04-19 14:45:12 +0800

    没有变化

--- src/MUSIC/dsp/dsp_asm.c
+++ src/MUSIC/dsp/dsp_asm.c
@@ -1,6 +1,15 @@
 #include "embARC.h"
 #include "embARC_debug.h"
 
+// long long _arc_macc64(long long sum, int x, int y) 
+// {
+
+// 	Asm("MACDF %0, %1" :"=r"(temp): "r"(val));
+
+	
+// 	return sum;
+// }
+
 
 
 #include "include.h"
