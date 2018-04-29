932099928415ccfc9ccabab5564b2df7272c085f
WUT Ruige_Lee <295054118@qq.com>
2018-04-29 11:03:06 +0800

    下载吃尽了资源，没法调度

--- src/TASK/NET_task.c
+++ src/TASK/NET_task.c
@@ -17,12 +17,12 @@ void net_task()
     // socket_request(SONG_ID);
     socket_request(SONG_INFO);
     // socket_request(SONG_INFO);
-    //download_mp3();
+    download_mp3();
     EMBARC_PRINTF("============================ while ============================\n");
 
     while (1)
     {
-        socket_request(SONG_INFO);
+        // socket_request(SONG_INFO);
     }
 
     return ;
