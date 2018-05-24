e675bf778a88a388d20bd185c6e3800082d2f1a8
Wuhan_University_of_Technology_Ruige_Lee <295054118@whut.edu.cn>
2018-05-24 10:07:44 +0800

    头文件注释

--- inc/include.h
+++ inc/include.h
@@ -13,34 +13,45 @@
 #include <stdlib.h>
 #include <malloc.h>
 
-/*********Global**********************/
+/**
+ * Global Definition
+ *  
+ */
+/* Define Net Buff and File Buff Size to 15MB */
 #define BUFF_SPACE 15728640
+
+/* Package Option */
 #define NET_LIST 0
 #define FILE_LIST 1
 
+/* Package Option */
 #define IN_FILE 0
 #define IN_BUFF 1
 
+/* Package Option */
+#define SONG_ID 0
+#define SONG_INFO 1
+
+/* Package Option */
+#define BUFF_EMPTY 0
+#define BUFF_FULL 1
+
+/* Package Events Bit */
 #define BIT_0	( 1 << 0 )
 #define BIT_1	( 1 << 1 )
 #define BIT_2	( 1 << 2 )
 #define BIT_3	( 1 << 3 )
 
-#define SONG_ID 0
-#define SONG_INFO 1
-
-#define BUFF_EMPTY 0
-#define BUFF_FULL 1
-
-/********define in filelist.c******/
+/**
+ * Define in filelist.c
+ * 
+ */
 struct filelist {
-	char data[50];
-	int32_t lenth;
-	uint8_t location;
-	struct filelist *next;
+	char data[50];				//Save Song ID or Song Name in different Function
+	int32_t lenth;				//Save length of Song File Buff For Decoder
+	uint8_t location;			//Mark Where Song Buff is,in SD Card or in Net Buff
+	struct filelist *next;		//Pointer of Linked List to Find Next Node
 };
-
-
 
 extern struct filelist *Songid_HEAD ;
 extern struct filelist *Songid_END ;
@@ -52,37 +63,51 @@ extern void filelist_init();
 extern void filelist_add(uint8_t list_id, char *id_data, int32_t lenth, uint8_t location);
 extern void filelist_delete(uint8_t list_id);
 
+/**
+ * Define in main.c
+ * 
+ */
+extern int32_t error_num ;		//Used to Get Error Num
 
-/***********define in main.c ************/
-
-extern int32_t error_num ;
-
-/***********define in mem.c ************/
-
+/**
+ * Define in mem.c
+ * 
+ */
 extern void readout_file(char *music_name);
 
-/***********define in music.c ************/
+/**
+ * Define in music.c
+ * 
+ */
 extern DEV_SPI_PTR spi;
 extern int8_t file_buff[ BUFF_SPACE ];
 extern void play_init();
 
-/***********define in dma2spi.c*********/
+/**
+ * Define in dma2spi.c
+ */
 extern void spi_dma_prepare(void);
 extern int32_t spi_writeraw(const void *data);
 extern int32_t Start_playing();
 
-/**************define in mp3api.c************/
+/**
+ * Define in mp3api.c
+ * 
+ */
 extern int32_t play_mp3(int32_t filelenth, uint8_t location);
 
-/**************define in iosignal.c**********/
-
+/**
+ * Define in iosignal.c
+ * 
+ */
 extern void iosignal_init();
 extern void iosignal_ctrl(uint8_t val, uint8_t num);
 extern uint8_t iosignal_read(uint8_t num);
 extern void net_rst();
 
-/*************define in net.c****************/
-
+/**
+ * Define in net.c
+ */
 extern uint8_t flag_netbuff;
 extern char dllink[500];
 extern char songpoint[50];
@@ -92,28 +117,32 @@ extern void net_init();
 extern int socket_request(uint8_t option);
 extern void download_mp3();
 
-/*********define in GUI.c************************/
+/**
+ * Define in GUI.c
+ */
 struct _gui_info {
-	uint8_t screen_point;
-	int16_t network_speed;
-	int16_t decord_speed;
+	uint8_t screen_point;				//Mark Which Page or Which Song is Pointed
+	int16_t network_speed;				//Pass Net Work Speed From Net Task to Gui Task
+	int16_t decord_speed;				//Pass Decoder Time From Music Task to Gui Task
 	int16_t main_cycle;
 
-	char *song_name;
-	char *next_song;
+	char *song_name;					//Pass Song Name From Music Task to Gui Task
+	char *next_song;					//Pass Next Song Name From Music Task to Gui Task
 
 	// uint8_t KEEP_ID3V2;
-	uint8_t flag_next;
+	uint8_t flag_next;					//Ask Music Task to Play Next Song
 
-	uint32_t delay_cnt;
+	uint32_t delay_cnt;					//Count Delay Tick to Reset Screen Pointer
 };
 
 extern struct _gui_info gui_info;
-
 extern void gui_init();
 extern void reflash_screen();
 
-/**********define in key.c********************/
+/**
+ * Define in key.c
+ * 
+ */
 extern void key1_isr();
 extern void key2_isr();
 extern void key3_isr();
